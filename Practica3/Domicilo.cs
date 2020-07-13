27a7286df65b4e4c673f4c5ba9f9193294556711
carlosguzmanjurado <carlos_viboraz@hotmail.com>
2020-07-13 19:57:29 +0200

    Primera

---
+++ Practica3/Domicilo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica03
+{
+    class Person
+    {
+        // four fields:
+        private string Calle;
+        private string Poblacion;
+        private string Provincia;
+        private int CodigoPostal;
+        private string Pais;
+
+        // Constructor that takes two argument:
+        public Person(string Calle, tring Calle, tring Calle, tring Calle, int CodigoPostal)
+        {
+            this.name = name;
+            this.age = age;
+            this.weight = 0;
+            this.height = 0;
+        }
+
+        // methods, getters and setters:
+        public void printPerson()
+        {
+            Console.WriteLine(this.name + " I am " + this.age + " years old");
+        }
+
+        public void becomeOlder()
+        {
+            this.age++;
+        }
+
+        public bool adult()
+        {
+            if (this.age < 18)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void settName(string name)
+        {
+            this.name = name;
+        }
+
+        public void ChangeHeight(int newHeight)
+        {
+            this.height = newHeight;
+        }
+    }
+}
---
+++ Practica3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica03
+{
+    class Person
+    {
+        // four fields:
+        private string name;
+        private int age;
+        private int height;
+        private int weight;
+
+        // Constructor that takes two argument:
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.weight = 0;
+            this.height = 0;
+        }
+
+        // methods, getters and setters:
+        public void printPerson()
+        {
+            Console.WriteLine(this.name + " I am " + this.age + " years old");
+        }
+
+        public void becomeOlder()
+        {
+            this.age++;
+        }
+
+        public bool adult()
+        {
+            if (this.age < 18)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void settName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
